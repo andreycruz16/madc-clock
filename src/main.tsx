b99abfc3866fafb1b181d7c9aa7b70c3dab35dc6
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-19 18:31:11 +0800

    implement vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { HelmetProvider } from "react-helmet-async";
 import { App } from "./app/App";
 import "./app/styles/index.css";
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </React.StrictMode>
 );
